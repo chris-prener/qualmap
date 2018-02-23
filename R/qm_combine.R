ad249c0dbdb0e2cf9beafd62a759721d080c5e6c
Christopher Prener <chris.prener@slu.edu>
2018-02-23 06:57:24 -0600

    update documentation for qm_combine

--- R/qm_combine.R
+++ R/qm_combine.R
@@ -1,5 +1,13 @@
 #' Combine objects
 #'
+#' @description A wrapper around \code{dplyr::bind_rows} for combining cluster objects into a single tibble.
+#'
+#' @param ... A list of cluster objects to be combined.
+#'
+#' @return A single tibble with all observations from the listed cluster objects.
+#'
+#' @importFrom dplr bind_rows
+#'
 #' @export
 qm_combine <- function(...){
 
