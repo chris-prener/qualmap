17a4aa906a061a0ad1f33a81b90f1ad75336f1b0
Christopher Prener <chris.prener@slu.edu>
2018-02-23 06:39:09 -0600

    further document the creation process

--- R/qm_create.R
+++ R/qm_create.R
@@ -1,11 +1,23 @@
 #' Create cluster object
 #'
+#' @description Each vector of input values is converted to a tibble organized in a "tidy" fashion.
+#'
+#' @details A cluster object contains a row for each feature in the reference data set. The \code{key} variable
+#' values are included in a variable named identically to the \code{key}. Additionally, a variable called
+#' \code{COUNT} is created. This will always be equal to \code{1} for each observation. It is intended to be
+#' used for summarization later in the data analysis process. Finally, three pieces of metadata are also included
+#' as arguments to provide data for subsetting later: a respondent identification number (\code{rid}), a
+#' cluster identification number (\code{cid}), and a category for the cluster type (\code{category}). These
+#' arguments are converted into values for the output variables \code{RID}, \code{CID}, and \code{CAT} respectively.
+#'
+#' @usage qm_create(ref, key, value, rid, cid, category, ...)
+#'
 #' @param ref An sf object that serves as a master list of features
 #' @param key Quoted name of id variable in the \code{ref} object to match input values to
 #' @param value A vector of input values
 #' @param ... An unquoted list of variables from the sf object to include in the out
 #'
-#' @return A tibble with the cluster values merged with elements of the key data.
+#' @return A tibble with the cluster values merged with elements of the reference data.
 #'
 #' @importFrom dplyr %>%
 #' @importFrom dplyr as_tibble
