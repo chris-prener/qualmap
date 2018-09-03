f3698a0cb3834377eb4bdf87cbfdf763d70f288e
Christopher Prener <chris.prener@slu.edu>
2018-09-02 20:12:50 -0500

    add useage and example to qm_define

--- R/qm_define.R
+++ R/qm_define.R
@@ -3,9 +3,14 @@
 #' @description A wrapper around \code{c()} that is used for constructing vectors of individual feature values.
 #' Each output should correspond to a single cluster on the respondent's map.
 #'
+#' @usage qm_define(...)
+#'
 #' @param ... A comma separated list of individual features
 #'
 #' @return A vector list each feature.
+#'
+#' @examples
+#' cluster <- qm_define(118600, 119101, 119300)
 #'
 #' @export
 qm_define <- function(...){
