6d1ac30f95794e8c65f1e81151a2b1a81bf1a49b
Christopher Prener <chris.prener@slu.edu>
2018-09-02 20:23:52 -0500

    add usage and example to qm_is_cluster

--- R/qm_is_cluster.R
+++ R/qm_is_cluster.R
@@ -2,10 +2,27 @@
 #'
 #' @description This function tests to see whether an object is of class \code{qm_cluster}.
 #'
+#' @usage qm_is_cluster(obj)
+#'
 #' @param obj Object to test
 #'
 #' @return A logical scalar that is \code{TRUE} is the given object is of class \code{qm_cluster};
 #' it will return \code{FALSE} otherwise.
+#'
+#' @examples
+#' # load and format reference data
+#' stl <- stLouis
+#' stl <- dplyr::mutate(stl, TRACTCE = as.numeric(TRACTCE))
+#'
+#' # create cluster
+#' cluster <- qm_define(118600, 119101, 119300)
+#'
+#' # create simple cluster object
+#' cluster_obj <- qm_create(ref = stl, key = TRACTCE, value = cluster,
+#'     rid = 1, cid = 1, category = "positive")
+#'
+#' # test cluster object
+#' qm_is_cluster(cluster_obj)
 #'
 #' @export
 qm_is_cluster <- function(obj){
